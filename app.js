e353f1b227d14317478ffea3a2bbafe0625fa270
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2023-03-08 13:40:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+const typed = new Typed('.multiple-text', {
+    strings:['Desenvolvedor Frontend...', 'Desenvolvedor Backend...'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
